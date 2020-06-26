5583328600bb9a770d8482369cdd2365be96db41
Romain Orhand <rorhand@unistra.fr>
2020-06-26 18:05:18 +0200

    Congrats ! Theoritical PEP values can be automatically computed for each maze

--- my_mazes/envs/abstract_maze.py
+++ my_mazes/envs/abstract_maze.py
@@ -100,6 +100,64 @@ class AbstractMaze(gym.Env):
                 transitions.append((node, action, neighbour))
         return transitions
 
+    def get_theoritical_probabilities(self):
+        # get all transitions
+        transitions = []
+        for node, action, neighbour in self.get_all_possible_transitions():
+            transitions.append((
+                "".join(self.maze.perception(*node)), 
+                action, 
+                "".join(self.maze.perception(*neighbour))
+            ))
+        # building the data structure by computing all reachable states depending on the action for every position
+        result = {}
+        for node, action, neighbour in transitions:
+            if node in result.keys():
+                if action in result[node].keys():
+                    result[node][action]["reachable_states"].append(neighbour)
+                else:
+                    result[node][action] = {"reachable_states":[neighbour]}
+            else :
+                result[node] = {action:{"reachable_states":[neighbour]}}
+        # completing the data structure with transitions that do not lead to a change in the environment
+        for key, value in result.items():
+            for action in ACTION_LOOKUP:
+                if action not in value.keys():
+                    value[action] = {"reachable_states":[key]}
+        # adding to the structure the theoritical probabilities to perceive each item
+        number_of_actions = len(ACTION_LOOKUP)
+        for _start_state, action_and_reachable_states in result.items():
+            for key in action_and_reachable_states:
+                action_and_reachable_states[key]["probabilities"] = {}
+                theoritical_probabilities = action_and_reachable_states[key]["probabilities"]
+                reachable_states = action_and_reachable_states[key]["reachable_states"]
+                # 1° set up the probabilities depending on slippery if the action done is the expected one
+                for a in ACTION_LOOKUP:
+                    theoritical_probabilities[ACTION_LOOKUP[a]] = {}
+                    for i in range(len(reachable_states)):
+                        if reachable_states[i][a] in theoritical_probabilities[ACTION_LOOKUP[a]]:
+                            theoritical_probabilities[ACTION_LOOKUP[a]][reachable_states[i][a]] += 1. - (number_of_actions -1 ) * self.prob_slippery / number_of_actions
+                        else:
+                            theoritical_probabilities[ACTION_LOOKUP[a]][reachable_states[i][a]] = 1. - (number_of_actions -1 ) * self.prob_slippery / number_of_actions
+                # 2° taking in consideration the aliasing states due to the perceptual aliasing issue 
+                for _, prob in action_and_reachable_states[key]["probabilities"].items():
+                    for symbol in prob:
+                        prob[symbol] /= len(reachable_states)
+                # 3° update the probabilities depending on slippery if the action done is not the expected one
+            slip_rate_by_action = self.prob_slippery / number_of_actions #(uniform distribution)
+            for selected_action in action_and_reachable_states:
+                theoritical_probabilities = action_and_reachable_states[selected_action]["probabilities"]
+                for slippery_action in ACTION_LOOKUP:
+                    if selected_action != slippery_action:
+                        slippery_reachable_states = action_and_reachable_states[slippery_action]["reachable_states"]
+                        for a in ACTION_LOOKUP:
+                            for i in range(len(slippery_reachable_states)):
+                                if slippery_reachable_states[i][a] in theoritical_probabilities[ACTION_LOOKUP[a]]:
+                                    theoritical_probabilities[ACTION_LOOKUP[a]][slippery_reachable_states[i][a]] += slip_rate_by_action / len(slippery_reachable_states)
+                                else:
+                                    theoritical_probabilities[ACTION_LOOKUP[a]][slippery_reachable_states[i][a]] = slip_rate_by_action / len(slippery_reachable_states)
+        return result
+
     def get_all_aliased_states(self):
         all_aliased_states = []
         for y in range(0, self.maze.max_y):
