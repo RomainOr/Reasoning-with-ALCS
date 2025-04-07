57c934937b877f51edb9e498e1a6a5bc25ef2217
Romain Orhand <rorhand@unistra.fr>
2025-04-07 18:59:47 +0200

    Continue refactor related to migration to Gymnasium

--- gymnasium_mazes/envs/maze_gym_env.py
+++ gymnasium_mazes/envs/maze_gym_env.py
@@ -30,32 +30,59 @@ class Actions(Enum):
     WEST = 6
     NORTHWEST = 7
 
-class MazeStructure:
-    """
-    Creates new maze.
-    """
+class MazeObservationSpace(gym.Space):
+    def __init__(self, n):
+        # n is the number of visible neighbour fields, typically 8
+        self.n = n
+        gym.Space.__init__(self, (self.n,), str)
 
-    def __init__(self, matrix, aliasing_matrix):
-        self.matrix = matrix
-        self.aliasing_matrix = aliasing_matrix
-        self.max_x = self.matrix.shape[1]
-        self.max_y = self.matrix.shape[0]
+    def sample(self):
+        return tuple(
+            self.np_random.choice(OBSERVATION_MAPPING.values()) for _ in range(self.n))
 
-    def get_possible_insertion_coordinates(self):
-        """
-        Returns a list with coordinates in the environment where
-        an agent can be placed (only on the path).
+    def contains(self, x):
+        return all(elem in (OBSERVATION_MAPPING.values()) for elem in x)
 
-        Returns
-        -------
-        list of tuples (X,Y) containing coordinates
-        """
-        possible_cords = []
-        for x in range(0, self.max_x):
-            for y in range(0, self.max_y):
-                if self.is_path(x, y):
-                    possible_cords.append((x, y))
-        return possible_cords
+
+class MazeGymEnv(gym.Env):
+    metadata = {'render_modes': ['human', 'ansi', 'aliasing_human'], "render_fps": 1}
+
+    def __init__(self, matrix, aliasing_matrix, render_mode='aliasing_human'):
+        self.maze = matrix
+        self.aliased_maze_to_plot = aliasing_matrix
+        self.max_x = self.maze.shape[1]
+        self.max_y = self.maze.shape[0]
+        self._agent_location = np.array([-1, -1], dtype=int)
+        self.action_space = gym.spaces.Discrete(len(Actions))
+        self._action_to_direction = {
+            Actions.NORTH.value: np.array([0, -1]),
+            Actions.NORTHEAST.value: np.array([1, -1]),
+            Actions.EAST.value: np.array([1, 0]),
+            Actions.SOUTHEAST.value: np.array([1, 1]),
+            Actions.SOUTH.value: np.array([0, 1]),
+            Actions.SOUTHWEST.value: np.array([-1, 1]),
+            Actions.WEST.value: np.array([-1, 0]),
+            Actions.NORTHWEST.value: np.array([-1, -1])
+        }
+        self.observation_space = MazeObservationSpace(8)
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        self._insert_animat()
+        return self._get_obs(), self._get_info()
+
+    def step(self, action):
+        previous_observation = self._get_obs()
+        if previous_observation[action] != str(OBSERVATION_MAPPING['WALL']):
+            self._agent_location += self._action_to_direction[action]
+        observation = self._get_obs()
+        reward = self._get_reward()
+        terminated = self._is_terminated()
+        info = self._get_info()
+        truncated = False
+        return observation, reward, terminated, truncated, info
 
     def build_perception_from_location(self, pos_x, pos_y):
         if not (0 <= pos_x < self.max_x):
@@ -66,51 +93,109 @@ class MazeStructure:
         if pos_y == 0:
             n = None
         else:
-            n = str(self.matrix[pos_y - 1, pos_x])
+            n = str(self.maze[pos_y - 1, pos_x])
         # Position NE
         if pos_x == self.max_x - 1 or pos_y == 0:
             ne = None
         else:
-            ne = str(self.matrix[pos_y - 1, pos_x + 1])
+            ne = str(self.maze[pos_y - 1, pos_x + 1])
         # Position E
         if pos_x == self.max_x - 1:
             e = None
         else:
-            e = str(self.matrix[pos_y, pos_x + 1])
+            e = str(self.maze[pos_y, pos_x + 1])
         # Position SE
         if pos_x == self.max_x - 1 or pos_y == self.max_y - 1:
             se = None
         else:
-            se = str(self.matrix[pos_y + 1, pos_x + 1])
+            se = str(self.maze[pos_y + 1, pos_x + 1])
         # Position S
         if pos_y == (self.max_y - 1):
             s = None
         else:
-            s = str(self.matrix[pos_y + 1, pos_x])
+            s = str(self.maze[pos_y + 1, pos_x])
         # Position SW
         if pos_x == 0 or pos_y == self.max_y - 1:
             sw = None
         else:
-            sw = str(self.matrix[pos_y + 1, pos_x - 1])
+            sw = str(self.maze[pos_y + 1, pos_x - 1])
         # Position W
         if pos_x == 0:
             w = None
         else:
-            w = str(self.matrix[pos_y, pos_x - 1])
+            w = str(self.maze[pos_y, pos_x - 1])
         # Position NW
         if pos_x == 0 or pos_y == 0:
             nw = None
         else:
-            nw = str(self.matrix[pos_y - 1, pos_x - 1])
+            nw = str(self.maze[pos_y - 1, pos_x - 1])
         return n, ne, e, se, s, sw, w, nw
 
-    def is_exit(self, pos_x, pos_y):
-        return self.matrix[pos_y, pos_x] == OBSERVATION_MAPPING['EXIT']
+    def _get_obs(self):
+        return self.build_perception_from_location(self._agent_location[0], self._agent_location[1])
+        
+    def _get_info(self):
+        return {}
 
-    def is_path(self, pos_x, pos_y):
-        return self.matrix[pos_y, pos_x] == OBSERVATION_MAPPING['PATH']
+    def _is_exit(self, pos_x, pos_y):
+        return self.maze[pos_y, pos_x] == OBSERVATION_MAPPING['EXIT']
 
-    def get_possible_neighbour_cords(self, pos_x, pos_y) -> tuple:
+    def _get_reward(self):
+        if self._is_exit(self._agent_location[0], self._agent_location[1]):
+            return 1000
+        return 0
+
+    def _is_terminated(self):
+        return self._is_exit(self._agent_location[0], self._agent_location[1])
+
+    def _is_path(self, pos_x, pos_y):
+        return self.maze[pos_y, pos_x] == OBSERVATION_MAPPING['PATH']
+
+    def _insert_animat(self):
+        possible_coords = []
+        for x in range(0, self.max_x):
+            for y in range(0, self.max_y):
+                if self._is_path(x, y):
+                    possible_coords.append((x, y))
+        starting_position = self.np_random.choice(possible_coords)
+        self._agent_location = np.array([starting_position[0], starting_position[1]], dtype=int)
+
+    def render(self):
+        if self.render_mode == 'aliasing_human':
+            return self._render_to_file(sys.stdout, aliasing_mode=True)
+        elif self.render_mode == 'human':
+            return self._render_to_file(sys.stdout, aliasing_mode=False)
+        elif self.render_mode == 'ansi':
+            output = io.StringIO()
+            self._render_to_file(output, aliasing_mode=False)
+            return output.getvalue()
+
+    def _render_to_file(self, outfile, aliasing_mode):
+        outfile.write("\n")
+        if aliasing_mode:
+            situation = np.copy(self.aliased_maze_to_plot)
+        else:
+            situation = np.copy(self.maze)
+        situation[self._agent_location[1], self._agent_location[0]] = OBSERVATION_MAPPING['ANIMAT']
+        for row in situation:
+            outfile.write(" ".join(self._render_element(el) for el in row))
+            outfile.write("\n")
+
+    def _render_element(self, el):
+        if el == OBSERVATION_MAPPING['WALL']:
+            return gym.utils.colorize('■', 'gray')
+        elif el == OBSERVATION_MAPPING['PATH']:
+            return gym.utils.colorize('□', 'white')
+        elif el == OBSERVATION_MAPPING['EXIT']:
+            return gym.utils.colorize('$', 'yellow')
+        elif el == OBSERVATION_MAPPING['ANIMAT']:
+            return gym.utils.colorize('A', 'red')
+        elif el == OBSERVATION_MAPPING['ALIASING']:
+            return gym.utils.colorize('■', 'cyan')
+        else:
+            return gym.utils.colorize(el, 'black')
+
+    def _get_possible_neighbour_cords(self, pos_x, pos_y) -> tuple:
         """
         Returns a tuple with coordinates for
         N, NE, E, SE, S, SW, W, NW neighbouring cells.
@@ -125,28 +210,28 @@ class MazeStructure:
         nw = (pos_x - 1, pos_y - 1)
         return n, ne, e, se, s, sw, w, nw
     
-    def create_graph(self):
+    def _create_graph(self):
         # Create uni-directed graph
         g = nx.Graph()
         # Add nodes
         for x in range(0, self.max_x):
             for y in range(0, self.max_y):
-                if self.is_path(x, y):
+                if self._is_path(x, y):
                     g.add_node((x, y), type='path')
-                if self.is_exit(x, y):
+                if self._is_exit(x, y):
                     g.add_node((x, y), type='exit')
         # Add edges
         path_nodes = [cords for cords, attribs
             in g.nodes(data=True) if attribs['type'] == 'path']
         for n in path_nodes:
-            neighbour_cells = self.get_possible_neighbour_cords(*n)
+            neighbour_cells = self._get_possible_neighbour_cords(*n)
             allowed_cells = [c for c in neighbour_cells
-                if self.is_path(*c) or self.is_exit(*c)]
+                if self._is_path(*c) or self._is_exit(*c)]
             edges = [(n, dest) for dest in allowed_cells]
             g.add_edges_from(edges)
         return g
 
-    def distinguish_direction(self, start, end):
+    def _distinguish_direction(self, start, end):
         direction = ''
         if end[1] + 1 == start[1]:
             direction += 'N'
@@ -157,6 +242,22 @@ class MazeStructure:
         if end[0] - 1 == start[0]:
             direction += 'E'
         return direction
+
+    def get_all_aliased_states(self):
+        all_aliased_states = []
+        for x in range(0, self.max_x):
+            for y in range(0, self.max_y):
+                if self.aliased_maze_to_plot[y, x] == OBSERVATION_MAPPING['ALIASING']:
+                    all_aliased_states.append(self.build_perception_from_location(x, y))
+        return list(dict.fromkeys(all_aliased_states))
+
+    def get_all_non_aliased_states(self):
+        all_non_aliased_states = []
+        for x in range(0, self.max_x):
+            for y in range(0, self.max_y):
+                if self.aliased_maze_to_plot[y, x] == OBSERVATION_MAPPING['PATH']:
+                    all_non_aliased_states.append(self.build_perception_from_location(x, y))
+        return all_non_aliased_states
 
     def get_all_possible_transitions(self):
         DIRECTION_LOOKUP = {
@@ -170,12 +271,12 @@ class MazeStructure:
             'NW': Actions.NORTHWEST.value
         }
         transitions = []
-        g = self.create_graph()
+        g = self._create_graph()
         path_nodes = (node for node, data
             in g.nodes(data=True) if data['type'] == 'path')
         for node in path_nodes:
             for neighbour in nx.all_neighbors(g, node):
-                direction = self.distinguish_direction(node, neighbour)
+                direction = self._distinguish_direction(node, neighbour)
                 transitions.append((node, DIRECTION_LOOKUP[direction], neighbour))
         return transitions
 
@@ -236,134 +337,3 @@ class MazeStructure:
                                 else:
                                     theoritical_probabilities[a.value][int(slippery_reachable_states[i][a.value])] = slip_rate_by_action / len(slippery_reachable_states)
         return result
-
-    def get_all_aliased_states(self):
-        all_aliased_states = []
-        for x in range(0, self.max_x):
-            for y in range(0, self.max_y):
-                if self.aliasing_matrix[y, x] == OBSERVATION_MAPPING['ALIASING']:
-                    all_aliased_states.append(self.build_perception_from_location(x, y))
-        return list(dict.fromkeys(all_aliased_states))
-
-    def get_all_non_aliased_states(self):
-        all_non_aliased_states = []
-        for x in range(0, self.max_x):
-            for y in range(0, self.max_y):
-                if self.aliasing_matrix[y, x] == OBSERVATION_MAPPING['PATH']:
-                    all_non_aliased_states.append(self.build_perception_from_location(x, y))
-        return all_non_aliased_states
-
-class MazeObservationSpace(gym.Space):
-    def __init__(self, n):
-        # n is the number of visible neighbour fields, typically 8
-        self.n = n
-        gym.Space.__init__(self, (self.n,), str)
-
-    def sample(self):
-        return tuple(
-            self.np_random.choice(OBSERVATION_MAPPING.values()) for _ in range(self.n))
-
-    def contains(self, x):
-        return all(elem in (OBSERVATION_MAPPING.values()) for elem in x)
-
-    def to_jsonable(self, sample_n):
-        return list(sample_n)
-
-    def from_jsonable(self, sample_n):
-        return tuple(sample_n)
-
-
-class MazeGymEnv(gym.Env):
-    metadata = {'render_modes': ['human', 'ansi', 'aliasing_human'], "render_fps": 1}
-
-    def __init__(self, matrix, aliasing_matrix, render_mode='aliasing_human'):
-        self.maze = MazeStructure(matrix,aliasing_matrix)
-        self._agent_location = np.array([-1, -1], dtype=int)
-        self.action_space = gym.spaces.Discrete(len(Actions))
-        self._action_to_direction = {
-            Actions.NORTH.value: np.array([0, -1]),
-            Actions.NORTHEAST.value: np.array([1, -1]),
-            Actions.EAST.value: np.array([1, 0]),
-            Actions.SOUTHEAST.value: np.array([1, 1]),
-            Actions.SOUTH.value: np.array([0, 1]),
-            Actions.SOUTHWEST.value: np.array([-1, 1]),
-            Actions.WEST.value: np.array([-1, 0]),
-            Actions.NORTHWEST.value: np.array([-1, -1])
-        }
-        self.observation_space = MazeObservationSpace(8)
-        assert render_mode is None or render_mode in self.metadata["render_modes"]
-        self.render_mode = render_mode
-
-    def reset(self, seed=None, options=None):
-        super().reset(seed=seed)
-        self._insert_animat()
-        return self._get_obs(), self._get_info()
-
-    def step(self, action):
-        previous_observation = self._get_obs()
-        if previous_observation[action] != str(OBSERVATION_MAPPING['WALL']):
-            self._agent_location += self._action_to_direction[action]
-        observation = self._get_obs()
-        reward = self._get_reward()
-        terminated = self._is_terminated()
-        info = self._get_info()
-        truncated = False
-        return observation, reward, terminated, truncated, info
-
-    def _get_obs(self):
-        return self.maze.build_perception_from_location(self._agent_location[0], self._agent_location[1])
-        
-    def _get_info(self):
-        return {}
-
-    def _get_reward(self):
-        if self.maze.is_exit(self._agent_location[0], self._agent_location[1]):
-            return 1000
-        return 0
-
-    def _is_terminated(self):
-        return self.maze.is_exit(self._agent_location[0], self._agent_location[1])
-
-    def _insert_animat(self):
-        possible_coords = []
-        for x in range(0, self.maze.max_x):
-            for y in range(0, self.maze.max_y):
-                if self.maze.is_path(x, y):
-                    possible_coords.append((x, y))
-        starting_position = self.np_random.choice(possible_coords)
-        self._agent_location = np.array([starting_position[0], starting_position[1]], dtype=int)
-
-    def render(self):
-        if self.render_mode == 'aliasing_human':
-            return self._render_to_file(sys.stdout, aliasing_mode=True)
-        elif self.render_mode == 'human':
-            return self._render_to_file(sys.stdout, aliasing_mode=False)
-        elif self.render_mode == 'ansi':
-            output = io.StringIO()
-            self._render_to_file(output, aliasing_mode=False)
-            return output.getvalue()
-
-    def _render_to_file(self, outfile, aliasing_mode):
-        outfile.write("\n")
-        if aliasing_mode:
-            situation = np.copy(self.maze.aliasing_matrix)
-        else:
-            situation = np.copy(self.maze.matrix)
-        situation[self._agent_location[1], self._agent_location[0]] = OBSERVATION_MAPPING['ANIMAT']
-        for row in situation:
-            outfile.write(" ".join(self._render_element(el) for el in row))
-            outfile.write("\n")
-
-    def _render_element(self, el):
-        if el == OBSERVATION_MAPPING['WALL']:
-            return gym.utils.colorize('■', 'gray')
-        elif el == OBSERVATION_MAPPING['PATH']:
-            return gym.utils.colorize('□', 'white')
-        elif el == OBSERVATION_MAPPING['EXIT']:
-            return gym.utils.colorize('$', 'yellow')
-        elif el == OBSERVATION_MAPPING['ANIMAT']:
-            return gym.utils.colorize('A', 'red')
-        elif el == OBSERVATION_MAPPING['ALIASING']:
-            return gym.utils.colorize('■', 'cyan')
-        else:
-            return gym.utils.colorize(el, 'black')
--- my_examples/metrics/MazeMetrics.py
+++ my_examples/metrics/MazeMetrics.py
@@ -32,11 +32,11 @@ def _maze_metrics(
             population,
             environment
         ) -> float:
-        transitions = environment.unwrapped.maze.get_all_possible_transitions()
+        transitions = environment.unwrapped.get_all_possible_transitions()
         env_trans = []
         for start, action, end in transitions:
-            p0 = environment.unwrapped.maze.build_perception_from_location(*start)
-            p1 = environment.unwrapped.maze.build_perception_from_location(*end)
+            p0 = environment.unwrapped.build_perception_from_location(*start)
+            p1 = environment.unwrapped.build_perception_from_location(*end)
             env_trans.append((p0, action, p1))
         # Take into consideration only reliable classifiers
         reliable_classifiers = [cl for cl in population if cl.is_reliable() and cl.behavioral_sequence is None]
@@ -62,7 +62,7 @@ def _how_many_eps_match_non_aliased_states(
         env
     ) -> int:
     counter = 0
-    non_aliased_perceptions = env.unwrapped.maze.get_all_non_aliased_states()
+    non_aliased_perceptions = env.unwrapped.get_all_non_aliased_states()
     enhanced_classifiers = [cl for cl in pop if cl.is_reliable() and cl.is_enhanced()]
     for percept in non_aliased_perceptions:
         for cl in enhanced_classifiers:
@@ -112,7 +112,7 @@ def _enhanced_effect_error(
         classifier_length,
         random_attribute_length
     ) -> float:
-    theoritical_probabilities = environment.unwrapped.maze.get_theoritical_probabilities()
+    theoritical_probabilities = environment.unwrapped.get_theoritical_probabilities()
     # Accumulation of difference in probabilities
     error_pep = 0.
     # For all possible destinations from each path cell
